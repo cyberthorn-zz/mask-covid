2cc4ac093cf630944977eb380eb5a617a183c5ae
Chadchavan Rattanasopa <chad.rattanasopa@gmail.com>
2021-01-16 03:05:07 +0700

    all area

--- first_app.py
+++ first_app.py
@@ -92,19 +92,181 @@ rama1_2.loc[0, 'total_minute'] = rama1_2.loc[0, 'total']
 rama1_2['datetime'] = rama1_2['date'].dt.date.astype(str) + " " + rama1_2['hour'].astype(str) +":" + rama1_2['minute'].astype(str)
 
 rama1_2 = rama1_2[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
-
 rama1_2.set_index('datetime', inplace=True)
+#--------------------------------------------------------------------------
+
+rajdamri_1 = mask_time[mask_time['name'] == "แนวถนนราชดำริ1"]
+rajdamri_1.reset_index(inplace=True)
+
+rajdamri_1['correct_wear_mask_minute'] = rajdamri_1['no_correct_wear_mask'].diff()
+rajdamri_1.loc[0, 'correct_wear_mask_minute'] = rajdamri_1.loc[0, 'no_correct_wear_mask']
+
+rajdamri_1['incorrect_wear_mask_minute'] = rajdamri_1['no_incorrect_wear_mask'].diff()
+rajdamri_1.loc[0, 'incorrect_wear_mask_minute'] = rajdamri_1.loc[0, 'no_incorrect_wear_mask']
+
+rajdamri_1['no_not_wear_mask_minute'] = rajdamri_1['no_not_wear_mask'].diff()
+rajdamri_1.loc[0, 'no_not_wear_mask_minute'] = rajdamri_1.loc[0, 'no_not_wear_mask']
+
+rajdamri_1['total_minute'] = rajdamri_1['total'].diff()
+rajdamri_1.loc[0, 'total_minute'] = rajdamri_1.loc[0, 'total']
+
+rajdamri_1['datetime'] = rajdamri_1['date'].dt.date.astype(str) + " " + rajdamri_1['hour'].astype(str) +":" + rajdamri_1['minute'].astype(str)
+
+rajdamri_1 = rajdamri_1[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+rajdamri_1.set_index('datetime', inplace=True)
+
+#------------------------------------------------------------
+rajdamri21 = mask_time[mask_time['name'] == "แนวถนนราชดำริ2-1"]
+rajdamri21.reset_index(inplace=True)
+
+rajdamri21['correct_wear_mask_minute'] = rajdamri21['no_correct_wear_mask'].diff()
+rajdamri21.loc[0, 'correct_wear_mask_minute'] = rajdamri21.loc[0, 'no_correct_wear_mask']
+
+rajdamri21['incorrect_wear_mask_minute'] = rajdamri21['no_incorrect_wear_mask'].diff()
+rajdamri21.loc[0, 'incorrect_wear_mask_minute'] = rajdamri21.loc[0, 'no_incorrect_wear_mask']
+
+rajdamri21['no_not_wear_mask_minute'] = rajdamri21['no_not_wear_mask'].diff()
+rajdamri21.loc[0, 'no_not_wear_mask_minute'] = rajdamri21.loc[0, 'no_not_wear_mask']
+
+rajdamri21['total_minute'] = rajdamri21['total'].diff()
+rajdamri21.loc[0, 'total_minute'] = rajdamri21.loc[0, 'total']
+
+rajdamri21['datetime'] = rajdamri21['date'].dt.date.astype(str) + " " + rajdamri21['hour'].astype(str) +":" + rajdamri21['minute'].astype(str)
+
+rajdamri21 = rajdamri21[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+rajdamri21.set_index('datetime', inplace=True)
+
+#-----------------------------------------------------------------
+rama1_1 = mask_time[mask_time['name'] == "แนวถนนพระราม1-1"]
+rama1_1.reset_index(inplace=True)
+
+rama1_1['correct_wear_mask_minute'] = rama1_1['no_correct_wear_mask'].diff()
+rama1_1.loc[0, 'correct_wear_mask_minute'] = rama1_1.loc[0, 'no_correct_wear_mask']
+
+rama1_1['incorrect_wear_mask_minute'] = rama1_1['no_incorrect_wear_mask'].diff()
+rama1_1.loc[0, 'incorrect_wear_mask_minute'] = rama1_1.loc[0, 'no_incorrect_wear_mask']
+
+rama1_1['no_not_wear_mask_minute'] = rama1_1['no_not_wear_mask'].diff()
+rama1_1.loc[0, 'no_not_wear_mask_minute'] = rama1_1.loc[0, 'no_not_wear_mask']
+
+rama1_1['total_minute'] = rama1_1['total'].diff()
+rama1_1.loc[0, 'total_minute'] = rama1_1.loc[0, 'total']
+
+rama1_1['datetime'] = rama1_1['date'].dt.date.astype(str) + " " + rama1_1['hour'].astype(str) +":" + rama1_1['minute'].astype(str)
+
+rama1_1 = rama1_1[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+rama1_1.set_index('datetime', inplace=True)
+
+#-------------------------------------------------------
+
+pharma1_2 = mask_time[mask_time['name'] == "หน้าองค์การเภสัช1-2"]
+pharma1_2.reset_index(inplace=True)
+
+pharma1_2['correct_wear_mask_minute'] = pharma1_2['no_correct_wear_mask'].diff()
+pharma1_2.loc[0, 'correct_wear_mask_minute'] = pharma1_2.loc[0, 'no_correct_wear_mask']
+
+pharma1_2['incorrect_wear_mask_minute'] = pharma1_2['no_incorrect_wear_mask'].diff()
+pharma1_2.loc[0, 'incorrect_wear_mask_minute'] = pharma1_2.loc[0, 'no_incorrect_wear_mask']
+
+pharma1_2['no_not_wear_mask_minute'] = pharma1_2['no_not_wear_mask'].diff()
+pharma1_2.loc[0, 'no_not_wear_mask_minute'] = pharma1_2.loc[0, 'no_not_wear_mask']
+
+pharma1_2['total_minute'] = pharma1_2['total'].diff()
+pharma1_2.loc[0, 'total_minute'] = pharma1_2.loc[0, 'total']
+
+pharma1_2['datetime'] = pharma1_2['date'].dt.date.astype(str) + " " + pharma1_2['hour'].astype(str) +":" + pharma1_2['minute'].astype(str)
+
+pharma1_2 = pharma1_2[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+pharma1_2.set_index('datetime', inplace=True)
+
+#-------------------------------------------------------------
+
+rajdamri2_2 = mask_time[mask_time['name'] == "แนวถนนราชดำริ2-2"]
+rajdamri2_2.reset_index(inplace=True)
+
+rajdamri2_2['correct_wear_mask_minute'] = rajdamri2_2['no_correct_wear_mask'].diff()
+rajdamri2_2.loc[0, 'correct_wear_mask_minute'] = rajdamri2_2.loc[0, 'no_correct_wear_mask']
+
+rajdamri2_2['incorrect_wear_mask_minute'] = rajdamri2_2['no_incorrect_wear_mask'].diff()
+rajdamri2_2.loc[0, 'incorrect_wear_mask_minute'] = rajdamri2_2.loc[0, 'no_incorrect_wear_mask']
+
+rajdamri2_2['no_not_wear_mask_minute'] = rajdamri2_2['no_not_wear_mask'].diff()
+rajdamri2_2.loc[0, 'no_not_wear_mask_minute'] = rajdamri2_2.loc[0, 'no_not_wear_mask']
+
+rajdamri2_2['total_minute'] = rajdamri2_2['total'].diff()
+rajdamri2_2.loc[0, 'total_minute'] = rajdamri2_2.loc[0, 'total']
+
+rajdamri2_2['datetime'] = rajdamri2_2['date'].dt.date.astype(str) + " " + rajdamri2_2['hour'].astype(str) +":" + rajdamri2_2['minute'].astype(str)
+
+rajdamri2_2 = rajdamri2_2[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+rajdamri2_2.set_index('datetime', inplace=True)
+
+#-------------------------------------------------------
+pharma1_1 = mask_time[mask_time['name'] == "หน้าองค์การเภสัช1-1"]
+pharma1_1.reset_index(inplace=True)
+
+pharma1_1['correct_wear_mask_minute'] = pharma1_1['no_correct_wear_mask'].diff()
+pharma1_1.loc[0, 'correct_wear_mask_minute'] = pharma1_1.loc[0, 'no_correct_wear_mask']
+
+pharma1_1['incorrect_wear_mask_minute'] = pharma1_1['no_incorrect_wear_mask'].diff()
+pharma1_1.loc[0, 'incorrect_wear_mask_minute'] = pharma1_1.loc[0, 'no_incorrect_wear_mask']
+
+pharma1_1['no_not_wear_mask_minute'] = pharma1_1['no_not_wear_mask'].diff()
+pharma1_1.loc[0, 'no_not_wear_mask_minute'] = pharma1_1.loc[0, 'no_not_wear_mask']
+
+pharma1_1['total_minute'] = pharma1_1['total'].diff()
+pharma1_1.loc[0, 'total_minute'] = pharma1_1.loc[0, 'total']
+
+pharma1_1['datetime'] = pharma1_1['date'].dt.date.astype(str) + " " + pharma1_1['hour'].astype(str) +":" + pharma1_1['minute'].astype(str)
+
+pharma1_1 = pharma1_1[['correct_wear_mask_minute', "incorrect_wear_mask_minute", "no_not_wear_mask_minute", "datetime" ]]
+
+pharma1_1.set_index('datetime', inplace=True)
+
 
 st.title('SuperAI deep application')
 st.header('DeepCare')
 # st.write(mask)
 st.write(mask)
 
+st.text("กราฟจำนวนผู้ใส่หน้ากาก ของแต่ละพื้นที่")
 st.bar_chart(maskgraph)
+
+st.text("กราฟสัดส้วนผู้ใส่หน้ากาก ของแต่ละพื้นที่")
 st.bar_chart(maskgraph_percent)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก ของตลาดทุ่งครุ ต่อ 1 นาที")
 st.bar_chart(toongkru)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนพระราม4-2 ต่อ 1 นาที")
 st.bar_chart(rama4_2)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนพระราม1-2 ต่อ 1 นาที")
 st.bar_chart(rama1_2)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนราชดำริ1 ต่อ 1 นาที")
+st.bar_chart(rajdamri_1)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนราชดำริ2_1 ต่อ 1 นาที")
+st.bar_chart(rajdamri21)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนพระราม1-1 ต่อ 1 นาที")
+st.bar_chart(rama1_1)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก หน้าองค์การเภสัช1-2 ต่อ 1 นาที")
+st.bar_chart(pharma1_2)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก แนวถนนราชดำริ2-2 ต่อ 1 นาที")
+st.bar_chart(rajdamri2_2)
+
+st.text("กราฟจำนวน ผู้ใส่หน้ากาก หน้าองค์การเภสัช1-1 ต่อ 1 นาที")
+st.bar_chart(pharma1_1)
+
 # c = alt.Chart(mask).mark_circle().encode(x='no_correct_wear_mask', y='no_incorrect_wear_mask', size='no_not_wear_mask', color='c', tooltip=['no_correct_wear_mask', 'no_incorrect_wear_mask', 'no_not_wear_mask'])
 
 # st.write(c)
